cf4e1604f19fd85245a406aca73800f9c81c4d5f
dtynn <dtynn@163.com>
2021-11-30 08:19:12 +0800

    sub1/v0.0.2

--- sub1/sub1.go
+++ sub1/sub1.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // Show infos about this module
 func Show() {
-	fmt.Println("inside github.com/dtynn/multimoddemo/sub1")
+	fmt.Println("inside github.com/dtynn/multimoddemo/sub1 in sub1/v0.0.2")
 }
